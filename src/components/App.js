42b7501c95f4fbe08cfeeeb74db2a580fda5dda2
Luciano Hayer <lucianohayer@gmail.com>
2021-08-15 19:25:56 +0200

    Add request to unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { images: [] };
+
+
+  onSearchSubmit = async (term) =>  {
+    try {
+      const response = await unsplash.get('/search/photos', {
+        params: { query: term }
+      });
+      
+      this.setState({images: response.data && response.data.results ? response.data.results : [] })
+
+
+    } catch(err) {
+      console.error('Error on fetch images');
+      this.setState({images: [] }) 
+    }
+    
+  } 
+  
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: { this.state.images.length } images
+      </div>
+    );  
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,17 @@ class SearchBar extends React.Component {
 
   state = { term: '' };
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.props);
+    
+    this.props.onSubmit(this.state.term);
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input type="text" value={ this.state.term } onChange={(e) => this.setState({ term: e.target.value } )} />
